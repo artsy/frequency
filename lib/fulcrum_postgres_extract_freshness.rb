3c18c62c83649d295bff6eee839b8d93f6a6192c
Emma Dickson <emma.dickson@artsymail.com>
2022-07-14 08:03:00 -0700

    update extracts

--- lib/fulcrum_postgres_extract_freshness.rb
+++ lib/fulcrum_postgres_extract_freshness.rb
@@ -7,23 +7,23 @@ require_relative './aws_helper'
 # Records timeliness of extracts tasks' results on S3
 class DataFreshness
   S3_LOCATIONS = {
-    account_activies: { bucket: 'artsy-data', prefix: 'reports/gravity.account_activies/' },
-    artist_series_artworks: { bucket: 'artsy-data', prefix: 'reports/gravity.artist_series_artworks/' },
-    artist_series: { bucket: 'artsy-data', prefix: 'reports/gravity.artist_series/' },
-    blocked_emails: { bucket: 'artsy-data', prefix: 'reports/gravity.blocked_emails/' },
-    collected_artworks: { bucket: 'artsy-data', prefix: 'reports/gravity.collected_artworks/' },
-    collections: { bucket: 'artsy-data', prefix: 'reports/gravity.collections/' },
-    commission_exemptions: { bucket: 'artsy-data', prefix: 'reports/gravity.commission_exemptions/' },
-    identity_verifications: { bucket: 'artsy-data', prefix: 'reports/gravity.identity_verifications/' },
-    jumio_scan_references: { bucket: 'artsy-data', prefix: 'reports/gravity.jumio_scan_references/' },
-    lots: { bucket: 'artsy-data', prefix: 'reports/gravity.lots/' },
-    lot_events: { bucket: 'artsy-data', prefix: 'reports/gravity.lot_events' },
-    rescource_notes: { bucket: 'artsy-data', prefix: 'reports/gravity.resource_notes' },
-    search_criteria: { bucket: 'artsy-data', prefix: 'reports/gravity.search_criteria' },
-    second_factors: { bucket: 'artsy-data', prefix: 'reports/gravity.second_factors' },
-    user_search_criterias: { bucket: 'artsy-data', prefix: 'reports/gravity.user_search_criteria' },
-    viewing_room_artworks: { bucket: 'artsy-data', prefix: 'reports/gravity.viewing_room_artworks' },
-    viewing_rooms: { bucket: 'artsy-data', prefix: 'reports/gravity.viewing_rooms' }
+    account_activies: { bucket: 'artsy-data', prefix: 'reports/gravity_account_activies/' },
+    artist_series_artworks: { bucket: 'artsy-data', prefix: 'reports/gravity_artist_series_artworks/' },
+    artist_series: { bucket: 'artsy-data', prefix: 'reports/gravity_artist_series/' },
+    blocked_emails: { bucket: 'artsy-data', prefix: 'reports/gravity_blocked_emails/' },
+    collected_artworks: { bucket: 'artsy-data', prefix: 'reports/gravity_collected_artworks/' },
+    collections: { bucket: 'artsy-data', prefix: 'reports/gravity_collections/' },
+    commission_exemptions: { bucket: 'artsy-data', prefix: 'reports/gravity_commission_exemptions/' },
+    identity_verifications: { bucket: 'artsy-data', prefix: 'reports/gravity_identity_verifications/' },
+    jumio_scan_references: { bucket: 'artsy-data', prefix: 'reports/gravity_jumio_scan_references/' },
+    lots: { bucket: 'artsy-data', prefix: 'reports/gravity_lots/' },
+    lot_events: { bucket: 'artsy-data', prefix: 'reports/gravity_lot_events' },
+    rescource_notes: { bucket: 'artsy-data', prefix: 'reports/gravity_resource_notes' },
+    search_criteria: { bucket: 'artsy-data', prefix: 'reports/gravity_search_criteria' },
+    second_factors: { bucket: 'artsy-data', prefix: 'reports/gravity_second_factors' },
+    user_search_criterias: { bucket: 'artsy-data', prefix: 'reports/gravity_user_search_criteria' },
+    viewing_room_artworks: { bucket: 'artsy-data', prefix: 'reports/gravity_viewing_room_artworks' },
+    viewing_rooms: { bucket: 'artsy-data', prefix: 'reports/gravity_viewing_rooms' }
   }.freeze
 
   def self.record_metrics
